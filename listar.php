2e61060cc6f875148ed983d97fbda54080fd76e8
Marlon Henrique <marlon_henrique2002@outlook.com>
2022-06-21 17:55:32 -0300

    Arrumando tela listar

--- listar.php
+++ listar.php
@@ -691,14 +691,7 @@ $query = mysqli_query($conect, $sql);
             $('#tabela-informacao').DataTable({
                 "scrollX": true,
                 "bDestroy": true,
-                "responsive": true,
-                "language": {
-                    "lengthMenu": "Display _MENU_ records per page",
-                    "zeroRecords": "Nothing found - sorry",
-                    "info": "Showing page _PAGE_ of _PAGES_",
-                    "infoEmpty": "No records available",
-                    "infoFiltered": "(filtered from _MAX_ total records)"
-                }             
+                "responsive": true,                           
             });
         });                                           
     </script>
